3b2ee8a5f07f4167f4422c37fd8c187e157b5bce
Ronaldo192 <80844881+Ronaldo192@users.noreply.github.com>
2023-12-26 12:05:11 -0300

    Create drop.sql

---
+++ drop.sql
@@ -0,0 +1,22 @@
+--Exclusão das tabela e respectivos relacionamentos 
+
+    DROP TABLE t_sgv_beneficio CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_categoria_pd CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_categoria_video CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_chamado CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_cliente CASCADE CONSTRAINTS;1
+    DROP TABLE t_sgv_departamento CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_funcionario CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_guest CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_nivel_beneficio CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_nivel_cliente CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_pf CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_pj CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_senha CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_produto CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_telefone CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_tel_func CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_tipo_fone CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_video CASCADE CONSTRAINTS;
+    DROP TABLE t_sgv_view CASCADE CONSTRAINTS;
+
